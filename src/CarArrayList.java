682ae4f26fb800a5d58c4df9a7403e0623001c04
Ken Henkel <KenHenkel95@gmail.com>
2019-02-06 17:00:20 -0500

    Worked further on method to find the average price for a given make and model within the ArrayList

--- src/CarArrayList.java
+++ src/CarArrayList.java
@@ -13,9 +13,22 @@ public class CarArrayList {
         carList.add(new Car("Hyundai", "Sonata", "2008", 200000.00, 3000.00));
 
         System.out.println(carList.toString());
+        System.out.println(getAveragePrice("Dodge", "Ram",carList));
     }
 
-    public static int getAveragePrice(ArrayList<Car> carList){
-
+    public static int getAveragePrice(String make, String model, ArrayList<Car> carList){
+        
+        int sum = 0;
+        int counter = 0;
+        for (int i = 0; i < carList.size(); i++){
+            if(carList.get(i).getMake().compareTo(make)==0){
+                if(carList.get(i).getModel().compareTo(model)==0){
+                    sum += carList.get(i).getPrice();
+                    counter++;
+                    if(carList.get(i).getPrice() > )
+                }
+            }
+        }
+        return sum/counter;
     }
 }
