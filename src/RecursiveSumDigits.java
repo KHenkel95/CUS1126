5d7e3951f2d1838d55b7c3452a0933e95890278d
Ken Henkel <KenHenkel95@gmail.com>
2019-02-07 16:46:50 -0500

    Added heading to RecursiveSumDigits

--- src/RecursiveSumDigits.java
+++ src/RecursiveSumDigits.java
@@ -1,3 +1,7 @@
+/* Author: Ken Henkel
+* Course: CUS1126
+* Date: 2/7/19
+ */
 public class RecursiveSumDigits {
     public static void main (String[] Args){
         System.out.println("The sum is: " + sumDigits(1234));
