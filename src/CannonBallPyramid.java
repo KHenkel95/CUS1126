6eb2e275f3d8efd8849a8bc6b330b75272b551b0
Ken Henkel <KenHenkel95@gmail.com>
2019-02-14 09:50:50 -0500

    Added headers to CannonBallPyramid and SubstringFinder classes

--- src/CannonBallPyramid.java
+++ src/CannonBallPyramid.java
@@ -1,4 +1,19 @@
+/* Author: Ken Henkel
+ * Course: CUS1126
+ * Date: 2/14/19
+ */
 public class CannonBallPyramid {
+    public static void main(String[] args){
+        int heightA = 3;
+        int heightB = 4;
+        int heightC = 5;
+        System.out.println("The number of cannonballs needed to make a pyramid with a height of "
+                + heightA + " is: " + countCannonballs(heightA));
+        System.out.println("The number of cannonballs needed to make a pyramid with a height of "
+                + heightB + " is: " + countCannonballs(heightB));
+        System.out.println("The number of cannonballs needed to make a pyramid with a height of "
+                + heightC + " is: " + countCannonballs(heightC));
+    }
     public static int countCannonballs(int height){
         if(height == 0){
             return height;
@@ -6,8 +21,5 @@ public class CannonBallPyramid {
         int n = (int)Math.pow(height, 2);
         return n + countCannonballs(height-1);
     }
-    public static void main(String[] args){
-        int heightA = 3;
-        System.out.println("The number of cannonballs needed to make a pyramid with a height of " + heightA + " is: " + countCannonballs(heightA));
-    }
+
 }
--- src/SubstringFinder.java
+++ src/SubstringFinder.java
@@ -1,7 +1,21 @@
+/* Author: Ken Henkel
+ * Course: CUS1126
+ * Date: 2/14/19
+ */
 import java.util.ArrayList;
 
 public class SubstringFinder {
+    public static void main(String[] args){
+        String a = "helo";
+        ArrayList<String> b = new ArrayList<String>();
+        generateSubstrings(a, b);
+        System.out.println(b);
+    }
     public static void generateSubstrings(String str, ArrayList<String> result){
-
+        for(int y = 0; y < str.length()-1; y++) {
+            for (int i = 1; i < str.length() - 1; i++) {
+                result.add(str.substring(y) + str.substring(i));
+            }
+        }
     }
 }
