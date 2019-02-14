782d8b4d889d32a23456a20b5ec84c78c9943286
Ken Henkel <KenHenkel95@gmail.com>
2019-02-13 23:48:12 -0500

    Completed countCannonballs method in CannonBallPyramid class

--- src/CannonBallPyramid.java
+++ src/CannonBallPyramid.java
@@ -1,5 +1,13 @@
 public class CannonBallPyramid {
     public static int countCannonballs(int height){
-        
+        if(height == 0){
+            return height;
+        }
+        int n = (int)Math.pow(height, 2);
+        return n + countCannonballs(height-1);
+    }
+    public static void main(String[] args){
+        int heightA = 3;
+        System.out.println("The number of cannonballs needed to make a pyramid with a height of " + heightA + " is: " + countCannonballs(heightA));
     }
 }
