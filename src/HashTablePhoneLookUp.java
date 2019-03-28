1f04428f559400ebb10e3a1344dd7c8bdeadb8ba
Ken Henkel <KenHenkel95@gmail.com>
2019-03-28 16:50:48 -0400

    added phoneNumberLookUp functionality

--- src/HashTablePhoneLookUp.java
+++ src/HashTablePhoneLookUp.java
@@ -1,18 +1,38 @@
 public class HashTablePhoneLookUp {
-    PhoneBook[] a = new PhoneBook[20];
+    public static PhoneBook[] a = new PhoneBook[20];
     public static int hashFunction(String name){
         int nameNumber = 0;
         for(int i = 0; i < name.length(); i++){
             nameNumber+=name.codePointAt(i);
         }
+        if(nameNumber > 20){
+            nameNumber = 20;
+        }
         return nameNumber % 20;
     }
-    public void insertPhoneNumber(PhoneBook person){
+    public static void insertPhoneNumber(PhoneBook person){
         int index = hashFunction(person.fullName);
-        a[index] = person;
-
+        if (a[index] != null) {
+            a[index] = person;
+        }
+        else{
+            a[index] = person.next;
+        }
+    }
+    public static String phoneNumberLookUp(String name){
+        return a[hashFunction(name)].phoneNumber;
     }
     public static void main(String[] args){
-
+    PhoneBook person1 = new PhoneBook("Jerry Garcia", "5161234567");
+    PhoneBook person2 = new PhoneBook("Bob Weir", "9174893095");
+    PhoneBook person3 = new PhoneBook("Phil Lesh", "5165960395");
+    PhoneBook person4 = new PhoneBook("Mickey Hart", "5169503945");
+    PhoneBook person5 = new PhoneBook("Bill Kreutzmann", "5168594309");
+    insertPhoneNumber(person1);
+    insertPhoneNumber(person2);
+    insertPhoneNumber(person3);
+    insertPhoneNumber(person4);
+    insertPhoneNumber(person5);
+    System.out.println("Searching for Phil Lesh :" + phoneNumberLookUp(person3.fullName));
     }
 }
