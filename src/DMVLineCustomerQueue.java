5bf459d9da84340d30d9d63148f22523ac45e001
Ken Henkel <KenHenkel95@gmail.com>
2019-03-27 22:48:44 -0400

    Fixed enqueue and displayQueue methods

--- src/DMVLineCustomerQueue.java
+++ src/DMVLineCustomerQueue.java
@@ -27,18 +27,18 @@ public class DMVLineCustomerQueue {
         else{
             System.out.println("Queue is as follows:");
             while(current != null){
-                System.out.print(""+current.fullname + "->");
-                current = current.link;
+                System.out.print(""+current.fullName + "->");
+                current = current.next;
             }
 
         }
     }
 
     public static void main(String[] Args){
-        DMVLineCustomer customer1 = new DMVLineCustomer(1, "Jack Doe", "License Renewal", 50.00);
-        DMVLineCustomer customer2 = new DMVLineCustomer(2, "Mary Jane", "Registration Plate collection", 7.20);
-        DMVLineCustomer customer3 = new DMVLineCustomer(3, "Killer Joe", "Address Change on License", 20.05);
-        DMVLineCustomer customer4 = new DMVLineCustomer(4, "Nikki Semantic", "License renewal", 120.00);
+        DMVLineCustomer customer1 = new DMVLineCustomer(1, "Jack Doe", "License Renewal", 50.00f);
+        DMVLineCustomer customer2 = new DMVLineCustomer(2, "Mary Jane", "Registration Plate collection", 7.20f);
+        DMVLineCustomer customer3 = new DMVLineCustomer(3, "Killer Joe", "Address Change on License", 20.05f);
+        DMVLineCustomer customer4 = new DMVLineCustomer(4, "Nikki Semantic", "License renewal", 120.00f);
 
         DMVLineCustomerQueue Queue1 = new DMVLineCustomerQueue();
         Queue1.enqueue(customer1);
