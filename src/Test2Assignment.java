769ae02107f24a5fe419971088f8ae1797a324f8
Ken Henkel <KenHenkel95@gmail.com>
2019-04-10 16:28:53 -0400

    created Node.java

--- src/Test2Assignment.java
+++ src/Test2Assignment.java
@@ -1,6 +1,6 @@
 public class Test2Assignment {
-    public static void linkedListMerger(Node tail1){
-        
+    public static void linkedListMerger(Node tail1, Node head2){
+        tail1.next = head2;
     }
     public static void main(String[] Args){
 
